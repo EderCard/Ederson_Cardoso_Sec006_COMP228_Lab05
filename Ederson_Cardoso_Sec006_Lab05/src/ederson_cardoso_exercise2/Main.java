92c69a032187963f9b57a148255c2137d0b50ae2
EderCard <edersoncard@gmail.com>
2019-11-26 14:35:59 -0500

    Initial Setup

---
+++ Ederson_Cardoso_Sec006_Lab05/src/ederson_cardoso_exercise2/Main.java
@@ -0,0 +1,30 @@
+package ederson_cardoso_exercise2;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("EmployeeInformation.fxml"));
+			
+			Scene scene = new Scene(root);
+
+			primaryStage.setTitle("Employee Information System");
+			
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
