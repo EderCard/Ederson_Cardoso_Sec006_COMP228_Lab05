e7642870b1a8ab9c14fb9faafc5a23d2d340b11d
EderCard <edersoncard@gmail.com>
2019-11-27 10:00:19 -0500

    Add Dental Payment output string

--- Ederson_Cardoso_Sec006_Lab05/src/ederson_cardoso_exercise1/DentalPaymentController.java
+++ Ederson_Cardoso_Sec006_Lab05/src/ederson_cardoso_exercise1/DentalPaymentController.java
@@ -1,7 +1,9 @@
 package ederson_cardoso_exercise1;
 
 import java.net.URL;
+import java.text.NumberFormat;
 import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -21,51 +23,98 @@ public class DentalPaymentController {
 	private URL location;
 
 	@FXML
-	private Label lblOutput;
+	private Label OutputLabel;
 
 	@FXML
-	private TextField txtName;
+	private TextField NameTextField;
 
 	@FXML
-	private TextField txtAddress;
+	private TextField AddressTextField;
 
 	@FXML
-	private CheckBox cbxFlossing;
+	private CheckBox FlossingCheckBox;
 
 	@FXML
-	private CheckBox cbxFilling;
+	private CheckBox FillingCheckBox;
 
 	@FXML
-	private CheckBox cbxRootCanal;
+	private CheckBox RootCanalCheckBox;
 
 	@FXML
-	private Button btnCalculate;
+	private Button CalculateButton;
 
 	@FXML
-	private RadioButton rbtSenior;
+	private RadioButton SeniorRadioButton;
 
 	@FXML
-	private ToggleGroup tglAge;
+	private ToggleGroup AgeToggleGroup;
 
 	@FXML
-	private RadioButton rbtKids;
+	private RadioButton KidsRadioButton;
 
 	@FXML
-	private RadioButton rbtAdult;
+	private RadioButton AdultRadioButton;
 
 	@FXML
-	private ComboBox<String> cbbProvince = new ComboBox<String>();
+	private ComboBox<String> ProvinceComboBox = new ComboBox<String>();
 
 	@FXML
-	void btnCalculateClick(ActionEvent event) {
-		
+	void CalculateButonClick(ActionEvent event) {
+		try {
+			double total = 0, discount = 0, hst = 0;
+
+			// Set total value
+			if (FlossingCheckBox.isSelected()) {
+				total += 20;
+			}
+			if (FillingCheckBox.isSelected()) {
+				total += 75;
+			}
+			if (RootCanalCheckBox.isSelected()) {
+				total += 150;
+			}
+
+			// Calculate discount
+			if (SeniorRadioButton.isSelected()) {
+				discount = total * 0.1;
+			} else {
+				if (KidsRadioButton.isSelected()) {
+					discount = total * 0.15;
+				}
+			}
+
+			// Set HST percentage value
+			if (ProvinceComboBox.getValue() == "Alberta") {
+				hst = 0.07;
+			} else if (ProvinceComboBox.getValue() == "Ontario") {
+				hst = 0.13;
+			} else if (ProvinceComboBox.getValue() == "Quebec") {
+				hst = 0.06;
+			}
+
+			NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+
+			// Build string output
+			String output = String.format("%12s %8s %n%12s %8s %n%12s %8s %n%12s %n%12s %8s", 
+					"Before tax:", currencyFormat.format(total).toString(), 
+					"Discount:", currencyFormat.format(discount).toString(),
+					"HST:", currencyFormat.format((total - discount) * hst).toString(),
+					"---------------------",
+					"Total:", currencyFormat.format(total - discount + ((total - discount) * hst))).toString();
+			
+			OutputLabel.setText(output);
+			
+		} catch (Exception ex) {
+			OutputLabel.setText("Error: " + ex.getMessage());
+		}
 
 	}
 
 	@FXML
 	void initialize() {
-		cbbProvince.getItems().addAll("Alberta", "Ontario", "Quebec");
-		cbbProvince.setValue("---Select---");
+		ProvinceComboBox.getItems().addAll("Alberta", "Ontario", "Quebec");
+		ProvinceComboBox.setValue("---Select---");
+		AdultRadioButton.setSelected(true);
 
 	}
 }
