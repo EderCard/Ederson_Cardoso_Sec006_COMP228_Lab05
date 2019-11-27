2ff60560218343ac04a7cca11c0031955e8b0974
EderCard <edersoncard@gmail.com>
2019-11-27 10:27:21 -0500

    Add EmployeeInformation GUI

---
+++ Ederson_Cardoso_Sec006_Lab05/src/ederson_cardoso_exercise2/EmployeeInformationController.java
@@ -0,0 +1,71 @@
+package ederson_cardoso_exercise2;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class EmployeeInformationController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField IdTextField;
+
+    @FXML
+    private TextField NameTextField;
+
+    @FXML
+    private TextField SalaryTextField;
+
+    @FXML
+    private DatePicker DateDatePicker;
+
+    @FXML
+    private Button AddButton;
+
+    @FXML
+    private Button DisplayAllButton;
+
+    @FXML
+    private Button ClearButton;
+
+    @FXML
+    private TextField SearchTextField;
+
+    @FXML
+    private Button SearchButton;
+
+    @FXML
+    void AddButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ClearButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DisplayAllButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SearchButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+}
